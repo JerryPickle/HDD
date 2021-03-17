a9b16e0eec3295884436a680b8eafb2428a607f6
Aerchecono <aerchecono@gmail.com>
2021-03-17 07:06:57 -0700

    Updated Character Generator Script
    Character generator now makes at least 7 decoys (1 decoy for each trait) and fill the remaining 20 with random characters.

--- KillDash/Assets/Scripts/Customer.cs
+++ KillDash/Assets/Scripts/Customer.cs
@@ -2,62 +2,138 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Customer : MonoBehaviour
+public class Customer 
 {
     int[] waitTimes = { 55, 45, 35 };
     enum Tips {Extra, Normal, None}
     enum Drinks {Coke, Juice, Milkshake, Orange_Juice, Garlic_Juice}
     enum Pizza {Cheese, Pepperoni, Pineapple, Meat_Lovers, The_Garlic_Special}
-    enum SideFood {Fries,Tots,Chips,Breadsticks,The_Garlic_Surprise}
+    enum SideFood {Fries, Tots, Chips, Breadsticks, The_Garlic_Surprise}
 
-    public int mood;
-    public string howTip;
-    public string favDrink;
-    public string favPizza;
-    public string favSideFood;
-    public string hatePizza;
-    public string hateDrink;
+    public static int CustomerCount = 0;
+    public int mood = -1;
+    public string howTip = "-1";
+    public string favDrink = "-1";
+    public string favPizza = "-1";
+    public string favSideFood = "-1";
+    public string hatePizza = "-1";
+    public string hateDrink = "-1";
     SpriteRenderer thisSprite;
+
+
+
 
     public void CreateCustomer()
     {
 
-        mood = waitTimes[Random.Range(0, 2)];
-        howTip = ((Tips)Random.Range(0, 2)).ToString("g");
-        int numDrink = Random.Range(0, 4);
-        favDrink = ((Drinks)numDrink).ToString("g");
-        int numPizza = Random.Range(0, 4);
-        favPizza = ((Pizza)numPizza).ToString("g");
-        favSideFood = ((SideFood)Random.Range(0, 4)).ToString("g");
-        int newNumPizza = Random.Range(0, 4);
-        int newNumDrink = Random.Range(0, 4);
-
-        while (newNumPizza == numPizza)
+        //Randomly Generate Attributes
+        if(mood == -1)
         {
-            newNumPizza = Random.Range(0, 4);
+            mood = waitTimes[Random.Range(0, 2)];
         }
-        hatePizza = ((Pizza)newNumPizza).ToString("g");
 
-        while (newNumDrink == numDrink)
+       
+        if(howTip == "-1")
         {
-            newNumDrink = Random.Range(0, 4);
+            howTip = ((Tips)Random.Range(0, 2)).ToString("g");
         }
-        hateDrink = ((Drinks)newNumDrink).ToString("g");
+        
 
-        /*print(mood);
-        print(howTip);
-        print(favDrink);
-        print(favPizza);
-        print(favSideFood);
-        print(hatePizza);
-        print(hateDrink);*/
+        if (favDrink == "-1")
+        {
+            if(hateDrink == "-1")
+            {
+                favDrink = ((Drinks)Random.Range(0, 4)).ToString("g");
+            }
+            else
+            {
+                favDrink = hateDrink;
+            }
+            while(favDrink == hateDrink)
+            {
+                favDrink = ((Drinks)Random.Range(0,4)).ToString("g");
+            }
+        }
+           
+
+        if(favPizza == "-1")
+        {
+            if (hatePizza == "-1")
+            {
+                favPizza = ((Pizza)Random.Range(0, 4)).ToString("g");
+            }
+            else
+            {
+                favPizza = hatePizza;
+            }
+            while (favPizza == hatePizza)
+            {
+                favPizza = ((Pizza)Random.Range(0, 4)).ToString("g");
+            }
+            
+        }
+        
+
+        if(favSideFood == "-1")
+        {
+            favSideFood = ((SideFood)Random.Range(0, 4)).ToString("g");
+        }
+
+
+       if (hatePizza == "-1")
+        {
+            if (favPizza == "-1")
+            {
+                hatePizza = ((Pizza)Random.Range(0, 4)).ToString("g");
+            }
+            else
+            {
+                hatePizza = favPizza;
+            }
+            while (hatePizza == favPizza)
+            {
+                hatePizza = ((Pizza)Random.Range(0,4)).ToString("g");
+            }
+            
+
+        }
+        
+       if (hateDrink == "-1")
+        {
+            if (favDrink == "-1")
+            {
+                hateDrink = ((Drinks)Random.Range(0, 4)).ToString("g");
+            }
+            else
+            {
+                hateDrink = favDrink;
+            }
+            while (hateDrink == favDrink)
+            {
+                hateDrink = ((Drinks)Random.Range(0, 4)).ToString("g");
+            }
+        }
+
+        // Adds one to the total customers created in the game.
+        CustomerCount++;
+
+        Debug.Log("Customer = " + CustomerCount);
+        Debug.Log("Mood = " + mood);
+        Debug.Log("How Tip = " +howTip);
+        Debug.Log("favDrink = " + favDrink);
+        Debug.Log("favPizza = " + favPizza);
+        Debug.Log("favSideFood = " + favSideFood);
+        Debug.Log("hatePizza = " + hatePizza);
+        Debug.Log("hateDrink = " + hateDrink);
 
     }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+        CreateCustomer();
     }
 
     // Update is called once per frame
--- KillDash/Assets/Scripts/Level1.cs
+++ KillDash/Assets/Scripts/Level1.cs
@@ -5,21 +5,131 @@ using UnityEngine;
 public class Level1 : MonoBehaviour
 {
 
-    private Customer badguy;
+
+    public List<Customer> CustomersList = new List<Customer>();
+    private Customer badguy = new Customer();
     private PlayerController player;
     [SerializeField]private Customer[] customerSprites;
-
+    [SerializeField]public int decoyRangeMax = 3;
+    [SerializeField]public int decoyRangeMin = 1;
+    [SerializeField] public int totalCustomers = 20;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        reshuffle(customerSprites);
-
-        badguy = customerSprites[0];
-        badguy.CreateCustomer();
-
+        InitializeGame();
 
     }
+
+
+    void InitializeGame()
+    {
+
+        // This function will create all of the games customers at random, it will also make a list with all of the customers, and it will make arrays of each trait type decoy.
+        // We can use these arrays to help decide what clues are good/bad/really too good of a clue, by seeing how many decoys are in each array.
+        
+        // Create the Baddy at Complete Random, and add him to a Mast list of Customers.
+        badguy.CreateCustomer();
+        CustomersList.Add(badguy);
+
+
+
+        //Create at least one other person that shares the same trait for every trait, and also add them to the list.
+        int favPizzaDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
+        Customer[] decoy1 = new Customer[favPizzaDecoys];
+
+        for (int i = 0; i < favPizzaDecoys; i++)
+        {
+            decoy1[i] = new Customer();
+            CustomersList.Add(decoy1[i]);
+            decoy1[i].favPizza = badguy.favPizza;
+            decoy1[i].CreateCustomer();
+        }
+
+        int favDrinkDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
+        Customer[] decoy2 = new Customer[favDrinkDecoys];
+
+        for (int i = 0; i < favDrinkDecoys; i++)
+        {
+            decoy2[i] = new Customer();
+            CustomersList.Add(decoy2[i]);
+            decoy2[i].favDrink = badguy.favDrink;
+            decoy2[i].CreateCustomer();
+        }
+
+        int favSideDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
+        Customer[] decoy3 = new Customer[favSideDecoys];
+
+        for (int i = 0; i < favSideDecoys; i++)
+        {
+            decoy3[i] = new Customer();
+            CustomersList.Add(decoy3[i]);
+            decoy3[i].favSideFood = badguy.favSideFood;
+            decoy3[i].CreateCustomer();
+        }
+
+        int HatePizzaDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
+        Customer[] decoy4 = new Customer[HatePizzaDecoys];
+
+        for (int i = 0; i < HatePizzaDecoys; i++)
+        {
+            decoy4[i] = new Customer();
+            CustomersList.Add(decoy4[i]);
+            decoy4[i].hatePizza = badguy.hatePizza;
+            decoy4[i].CreateCustomer();
+        }
+
+        int HateDrinkDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
+        Customer[] decoy5 = new Customer[HateDrinkDecoys];
+
+        for (int i = 0; i < HateDrinkDecoys; i++)
+        {
+            decoy5[i] = new Customer();
+            CustomersList.Add(decoy5[i]);
+            decoy5[i].hateDrink = badguy.hateDrink;
+            decoy5[i].CreateCustomer();
+        }
+
+        int HowTipDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
+        Customer[] decoy6 = new Customer[HowTipDecoys];
+
+        for (int i = 0; i < HowTipDecoys; i++)
+        {
+            decoy6[i] = new Customer();
+            CustomersList.Add(decoy6[i]);
+            decoy6[i].howTip =  badguy.howTip;
+            decoy6[i].CreateCustomer();
+        }
+
+        int MoodDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
+        Customer[] decoy7 = new Customer[MoodDecoys];
+
+        for (int i = 0; i < MoodDecoys; i++)
+        {
+            decoy7[i] = new Customer();
+            CustomersList.Add(decoy7[i]);
+            decoy7[i].mood = badguy.mood;
+            decoy7[i].CreateCustomer();
+        }
+
+
+        // Figure out how many Customers are left to fill the roster and create that many at random. Then add them to the list.
+
+        int customersRemaining = totalCustomers - Customer.CustomerCount;
+        Customer[] customerFill = new Customer[customersRemaining]; 
+
+        for (int i = 0; i < customersRemaining; i++)
+        {
+            customerFill[i] = new Customer();
+            CustomersList.Add(customerFill[i]);
+            customerFill[i].CreateCustomer();
+        
+        }
+        
+        
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -28,15 +138,5 @@ public class Level1 : MonoBehaviour
     }
 
 
-    void reshuffle(Customer[] bois)
-    {
-        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
-        for (int t = 0; t < bois.Length; t++)
-        {
-            Customer tmp = bois[t];
-            int r = Random.Range(t, bois.Length);
-            bois[t] = bois[r];
-            bois[r] = tmp;
-        }
-    }
+   
 }
