850ed7d6d0c43b762b46b68e6ecf30734ddf9bf7
Nathan Maroko <nathanmaroko@u.boisestate.edu>
2021-03-17 12:03:20 -0600

    Fixed CustomerPrefabs

--- KillDash/Assets/Scripts/Level1.cs
+++ KillDash/Assets/Scripts/Level1.cs
@@ -7,10 +7,10 @@ public class Level1 : MonoBehaviour
 
 
     public List<Customer> CustomersList = new List<Customer>();
-    private Customer badguy = new Customer();
+    private Customer badguy;
     private PlayerController player;
     int currentCustomers = 0;
-    [SerializeField]private Customer[] customerSprites;
+    public Customer[] customerPrefabs;
     [SerializeField]public int decoyRangeMax = 3;
     [SerializeField]public int decoyRangeMin = 1;
     [SerializeField] public int totalCustomers = 20;
@@ -38,86 +38,86 @@ public class Level1 : MonoBehaviour
 
         //Create at least one other person that shares the same trait for every trait, and also add them to the list.
         int favPizzaDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
-        Customer[] decoy1 = new Customer[favPizzaDecoys];
 
         for (int i = 0; i < favPizzaDecoys; i++)
         {
-            decoy1[i] = new Customer();
-            decoy1[i].CreateCustomer();
-            decoy1[i].favPizza = badguy.favPizza;
-            CustomersList.Add(decoy1[i]);
+            Customer newCustomer;
+            newCustomer = customerPrefabs[currentCustomers];
+            newCustomer.CreateCustomer();
+            newCustomer.favPizza = badguy.favPizza;
+            CustomersList.Add(newCustomer);
             currentCustomers++;
         }
 
         int favDrinkDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
-        Customer[] decoy2 = new Customer[favDrinkDecoys];
 
         for (int i = 0; i < favDrinkDecoys; i++)
         {
-            decoy2[i] = new Customer();
-            decoy2[i].CreateCustomer();
-            decoy2[i].favDrink = badguy.favDrink;
-            CustomersList.Add(decoy2[i]);
+            Customer newCustomer;
+            newCustomer = customerPrefabs[currentCustomers];
+            newCustomer.CreateCustomer();
+            newCustomer.favDrink = badguy.favDrink;
+            CustomersList.Add(newCustomer);
             currentCustomers++;
         }
 
         int favSideDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
-        Customer[] decoy3 = new Customer[favSideDecoys];
 
         for (int i = 0; i < favSideDecoys; i++)
         {
-            decoy3[i] = new Customer();
-            decoy3[i].CreateCustomer();
-            decoy3[i].favSideFood = badguy.favSideFood;
-            CustomersList.Add(decoy3[i]);
+            Customer newCustomer;
+            newCustomer = customerPrefabs[currentCustomers];
+            newCustomer.CreateCustomer();
+            newCustomer.favSideFood = badguy.favSideFood;
+            CustomersList.Add(newCustomer);
             currentCustomers++;
         }
 
         int HatePizzaDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
-        Customer[] decoy4 = new Customer[HatePizzaDecoys];
 
         for (int i = 0; i < HatePizzaDecoys; i++)
         {
-            decoy4[i] = new Customer();
-            decoy4[i].CreateCustomer();
-            decoy4[i].hatePizza = badguy.hatePizza;
-            CustomersList.Add(decoy4[i]);
+            Customer newCustomer;
+            newCustomer = customerPrefabs[currentCustomers];
+            newCustomer.CreateCustomer();
+            newCustomer.hatePizza = badguy.hatePizza;
+            CustomersList.Add(newCustomer);
             currentCustomers++;
         }
 
         int HateDrinkDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
-        Customer[] decoy5 = new Customer[HateDrinkDecoys];
 
         for (int i = 0; i < HateDrinkDecoys; i++)
         {
-            decoy5[i] = new Customer();
-            decoy5[i].CreateCustomer();
-            decoy5[i].hateDrink = badguy.hateDrink;
-            CustomersList.Add(decoy5[i]);
+            Customer newCustomer;
+            newCustomer = customerPrefabs[currentCustomers];
+            newCustomer.CreateCustomer();
+            newCustomer.hateDrink = badguy.hateDrink;
+            CustomersList.Add(newCustomer);
             currentCustomers++;
         }
 
         int HowTipDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
-        Customer[] decoy6 = new Customer[HowTipDecoys];
 
         for (int i = 0; i < HowTipDecoys; i++)
         {
-            decoy6[i] = new Customer();
-            decoy6[i].CreateCustomer();
-            decoy6[i].howTip =  badguy.howTip;
-            CustomersList.Add(decoy6[i]);
+            Customer newCustomer;
+            newCustomer = customerPrefabs[currentCustomers];
+            newCustomer.CreateCustomer();
+            newCustomer.howTip = badguy.howTip;
+            CustomersList.Add(newCustomer);
             currentCustomers++;
         }
 
         int MoodDecoys = Random.Range(decoyRangeMin, decoyRangeMax);
-        Customer[] decoy7 = new Customer[MoodDecoys];
 
         for (int i = 0; i < MoodDecoys; i++)
         {
-            decoy7[i] = new Customer();
-            decoy7[i].CreateCustomer();
-            decoy7[i].mood = badguy.mood;
-            CustomersList.Add(decoy7[i]);
+            Customer newCustomer;
+            newCustomer = customerPrefabs[currentCustomers];
+            newCustomer.CreateCustomer();
+            newCustomer.mood = badguy.mood;
+            CustomersList.Add(newCustomer);
             currentCustomers++;
         }
 
@@ -125,13 +125,14 @@ public class Level1 : MonoBehaviour
         // Figure out how many Customers are left to fill the roster and create that many at random. Then add them to the list.
 
         int customersRemaining = totalCustomers - currentCustomers;
-        Customer[] customerFill = new Customer[customersRemaining]; 
 
         for (int i = 0; i < customersRemaining; i++)
         {
-            customerFill[i] = new Customer();
-            customerFill[i].CreateCustomer();
-            CustomersList.Add(customerFill[i]);
+            Customer newCustomer;
+            newCustomer = customerPrefabs[currentCustomers];
+            newCustomer.CreateCustomer();
+            CustomersList.Add(newCustomer);
+            currentCustomers++;
 
         }
         
