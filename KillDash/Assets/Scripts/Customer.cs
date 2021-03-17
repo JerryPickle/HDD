14340690a50381ad3f2f444323156862bf5b5696
Nathan Maroko <nathanmaroko@u.boisestate.edu>
2021-03-16 21:38:40 -0600

    Customer Script

---
+++ KillDash/Assets/Scripts/Customer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    enum WaitTime : ushort {GoodMood = 55, Neutral = 45, BadMood = 35 } 
+
+    private void CreateCustomer()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
